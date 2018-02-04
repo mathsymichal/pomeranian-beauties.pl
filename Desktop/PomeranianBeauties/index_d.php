c3bd458b4ec3a2aeec84b96a1ead92cc33149511
mathsymichal <michal.przykucki@gmail.com>
2018-02-04 22:43:14 +0000

    'odjazd'

---
+++ Desktop/PomeranianBeauties/index_d.php
@@ -0,0 +1,53 @@
+<?
+
+print ("
+
+	<div class=\"obraz0\">
+
+		<center><img src=\"cav0.jpg\" alt=\"Pomeranian Beauties\"></center>
+
+ <p>
+Ich begruesse herzlich Alle, die auf meine HP ueber die bezauberte Rasse CAVALIER KING CHARLES SPANIEL gefunden haben!!! Wenn ihr von einem Hund traeumt, der in Euch totall  verliebt sein wird, fuer den ihr, der Gott auf Erden sein werdet, der gleichzeitig ein lebhafter, froehlicher Spielgefaehrte fuer die Kinder sein wird, wie auch ein Hund den man einfach an die Seele druecken kann, wenn`s mal schief geht! Egal, ob ihr in der Stadt, oder auf dem Land lebt, im Haeuschen mit Garten oder im Block - da seit ihr hier richtig!
+ </p>
+
+ <p>
+Bei uns in Polen gibt es kaum, oder gar nicht, Monographiehen in polnischer Sprache ueber den Cavalier. Wir muessen uns mit Buechern in deutsch, englisch oder tschechisch weiterhelfen. Ich moechte nicht viel ueber die Geschichte der Rasse und den Standard schreiben, na vielleicht paar Saetze aus dem Buch von Ken Town, der seit ueber 30 Jahre die Cavaliere zuechtet, ausstellt und auch internationaler Richter der Rasse ist.
+ </p>
+
+ <p>
+\" Im Jahre 1928 wurde auf Crufts Dog Show der Cavalier King Charles Spaniel Club gegruendet. In der Gruendungsversammlung des Cavalier Club wurde der Name CAVALIER KING CHARLES SPANIEL gewaehlt, um die Rasse von ihrem kurznasigen Vetter zu unterscheiden. Damals wurde , auf Grund von  vielen Bildern von Toy Spaniels und dem Rueden Ann`s Son, der als lebendes Modell diente,  der Rassestandard gestaltet. So wurde der Rassestandard formuliert, den verschiedenen erwuenschten Merkmalen Punkte zugerechnet, die zusammengezaehlt die Zahl hundert ergaben. Dabei wurde grosser Nachdruck auf die Kopfform gelegt, dieser wurden nicht weniger als 55 Punkte zugeschrieben, einschliesslich der verschiedenen Teile wie Augen, Ohren, Oberkopf etc., dabei wurde unter ALLGEMEINERSCHEINUNG die Forderung aufgestellt, diese Hunde sollten aktiv, sportlich und furchtlos sein.\"
+ </p>
+
+ <p>
+Heutiger Rassestandart FCI Nr 136 vom 10.03.1988 sagt bei dem allgemeinem Erscheinungsbild:\" Aktiv, anmutig und ausgewogen, mit sanftem Ausdruck\" - und so ist der kleine Cavalier, er ist lieb, suess, auch sehr unternehmungslustig, liebevoll und sollte absolut furchtlos sein!
+ </p>
+
+ <p>
+Die Cavaliere gibt es in vier Farben:
+ </p>
+
+ <p>
+Blenheims ( lohfarben mit weiss ) - das ist die haeufigste Farbe - gut unterbrochene intensiv kastanienrote Markierungen auf perlweisser Grundfarbe. Markierungen am Kopf gleichmaessig verteilt mit ausrechendem Zwischenraum fuer die  \"Lozenge\" oder \"Spot - Markierung\" - die kleine , runde Markierung oben auf dem Kopf.  Im Ausstellungsring koennten Markierungen , die nicht symmetrisch verlaufen oder sich uebertrieben in das Gesicht ausdehnen, bestraft werden.
+ </p>
+
+ <p>
+Tricolours - gut verteiltes und gut unterbrochenes schwarz - weiss mit lohfarbenen Abzeichen ueber den Augen, an den Wangen, an der Innenseite der Ohren, an der Innenseite der Laeufe und an der Innenseite der Rute. Das Fehlen von Tan an einer dieser Stellen wuerde im Ausstellungsring bestraft.
+ </p>
+
+ <p>
+Unsere Lieblingsfarben sind ruby und black and tan!!! Die nennt man manchmal auch ganzfarbig aufgrund der fehlenden weissen Markierungen!
+ </p>
+
+ <p>
+Rubies - leuchtend rote Farbe ueber dem gesamten Koerper, weisse Flecken sind unerwuenscht.
+ </p>
+
+ <p>
+Black and Tan -  Rabenschwarz mit lohfarbenen Abzeichen  ueber den Augen, an den Wangen, an der Innenseite der Ohren, an der Brust, an den Laeufen und an der Unterseite der Rute. Die Lohfarbe sollte leuchtend sein. Weisse Flecken sind unerwuenscht.<br />&nbsp;
+ </p>
+
+	</div>
+	
+	");
+	
+?>
---
+++ Desktop/PomeranianBeauties/veterans_e.php
@@ -0,0 +1,109 @@
+<?
+
+print ("
+
+  <div class=\"newsy\">
+
+  <center>
+    <font color=\"navy\" size=\"4\">
+      <br /><b>Our Veterans</b>
+    </font>
+  </center><br />
+
+    <div class=\"news\">
+	<center>
+
+	<font color=\"black\" size=\"5\"><b>RUMBA Pomeranian Beauties</b></font><br /><br />
+
+	<font color=\"black\" size=\"4\"><b>born 28.07.2012</b></font><br /><br />
+
+	<font color=\"navy\" size=\"4\"><b>Champion of Poland</b></font><br /><br />
+
+	<img src=\"./psy/galeria/veterans/rumba.jpg\" alt=\"RUMBA Pomeranian Beauties\"></center><br />
+	</center>		
+    </div>
+	
+    <div class=\"news\">
+	<center>
+
+	<font color=\"black\" size=\"5\"><b>CASSANDRA vom Paulinenhof \"Casia\"</b></font><br /><br />
+
+	<font color=\"black\" size=\"4\"><b>born 13.05.2007 r.</b></font><br /><br />
+
+	<img src=\"./psy/galeria/veterans/casia.jpg\" alt=\"CASSANDRA vom Paulinenhof\"></center><br />
+	</center>		
+    </div>
+
+    <div class=\"news\">
+	<center>
+
+	<font color=\"black\" size=\"5\"><b>DAYLIGHT Pomeranian Beauties \"Dayla\"</b></font><br /><br />
+
+	<font color=\"black\" size=\"4\"><b>born 26.04.2008 r.</b></font><br /><br />
+
+	<font color=\"navy\" size=\"4\"><b>Champion of Poland and Russia</b></font><br />
+	<font color=\"navy\" size=\"4\"><b>Junior Champion of Poland</b></font><br /><br />
+
+	<img src=\"./psy/galeria/veterans/dayla.jpg\" alt=\"DAYLIGHT Pomeranian Beauties\"></center><br />
+	</center>		
+    </div>
+  
+  <div class=\"news\">
+	<center>
+
+	<font color=\"black\" size=\"5\"><b>CALYPSO Pomeranian Beauties \"Coca\"</b></font><br /><br />
+
+	<font color=\"black\" size=\"4\"><b>born 05.08.2007</b></font><br /><br />
+
+	<font color=\"navy\" size=\"4\"><b>Champion of Poland</b></font><br />
+<br />
+
+	<img src=\"./psy/galeria/veterans/coca.jpg\" alt=\"/>CALYPSO Pomeranian Beauties\"></center><br />
+	</center>		
+    </div>
+
+    <div class=\"news\">
+	<center>
+
+	<font color=\"black\" size=\"5\"><b>Royal Hero's HARPO</b></font><br /><br />
+
+	<font color=\"black\" size=\"4\"><b>born: 6.12.2003</b></font><br /><br />
+
+	<font color=\"navy\" size=\"4\"><b>Champion of Poland, Germany and Luxemburg</b></font><br /><br />
+
+	<img src=\"./psy/galeria/veterans/harpo.jpg\" alt=\"Royal Hero's HARPO\"></center><br />
+	</center>		
+    </div>
+
+    <div class=\"news\">
+	<center>
+
+	<font color=\"black\" size=\"5\"><b>NORMA JEAN du château noblesse</b></font><br /><br />
+
+	<font color=\"black\" size=\"4\"><b>born: 27.06.2005</b></font><br /><br />
+
+	<font color=\"navy\" size=\"4\"><b>Champion of Poland</b></font><br />
+	<font color=\"navy\" size=\"4\"><b>Junior Champion of Germany</b></font><br /><br />
+
+	<img src=\"./psy/galeria/veterans/norma.jpg\" alt=\"NORMA JEAN du château noblesse\"></center><br />
+	</center>		
+    </div>
+
+    <div class=\"news\">
+	<center>
+
+	<font color=\"black\" size=\"5\"><b>SILVER WOMAN van Cavino \"Silka\"</b></font><br /><br />
+
+	<font color=\"black\" size=\"4\"><b>born: 4.12.2005</b></font><br /><br />
+
+	<font color=\"navy\" size=\"4\"><b>Champion of Poland</b></font><br /><br />
+
+	<img src=\"./psy/galeria/veterans/silka.jpg\" alt=\"SILVER WOMAN van Cavino\"></center><br />
+	</center>		
+    </div>
+
+  </div>
+
+");
+
+?>
