b2ae28be71b8062e333bbd2b7f6c5835e81b4587
mathsymichal <36140040+mathsymichal@users.noreply.github.com>
2018-02-04 22:30:14 +0000

    Add files via upload

---
+++ about_e.php
@@ -0,0 +1,29 @@
+<?
+
+print ("
+
+	<div class=\"newsy\">
+
+	<div class=\"news\">
+	
+		<center><img src=\"my5.jpg\" alt=\"\"></center>	
+
+		<p>Hello and welcome to our countryside home! We live here in Wiślinka, a village placed 10km from Gdansk, and not far from the Sobieszewska Island (4km away from the sea). Together with us, i.e. with me - Ewa, and with Witek, lives our band of Cavaliers that you might get to know better on this website, as well as a Griffon Bruxellois \"Toruś\" (Torquito Boy Molidejs)... Our Only Child!</p>
+		
+		<center><img src=\"toro0.jpg\" alt=\"\"></center>
+
+		<p>Sometimes, during his visits to Poland, my son Michał stays with us. He's been living in the UK for a while now where he obtained his PhD degree in mathematics at the University of Cambridge (I had to mention this, I'm so proud!) and now he's working at the University of Oxford. However, we always could and still can count on his help with taking care about our website, with our dogs and with popularizing our wonderful breed!</p>
+
+		<center><img src=\"my6.jpg\" alt=\"\"></center>
+
+		<p>Both Witek and I are veterinary physician. We've been working with animals for many years and a few years in the Veterinary Inspection as well. Witek is currently retired and I'm also temporarily on a pension... Our education and experience are very helpful when we look after our dogs!</p>
+
+		<p>If you would like to find out more about the Cavalier King Charles Spaniels or about our dogs, please get in touch with us! You can call us on 0 48 58 691 3947 or +48 661 604 107, or drop us an <a href=\"mailto:Pomeranian Beauties <ewaiwitek@poczta.fm>\">e-m@il</a>. No question will remain unanswered!</p>
+		
+	</div>
+
+	</div>
+	
+	");
+
+?>
---
+++ album_d.php
@@ -0,0 +1,14 @@
+<?
+
+print ("
+
+
+	<div class=\"teksty\">
+	
+	<center><embed type=\"application/x-shockwave-flash\" src=\"https://picasaweb.google.com/s/c/bin/slideshow.swf\" width=\"600\" height=\"400\" flashvars=\"host=picasaweb.google.com&hl=pl&feat=flashalbum&interval=5&RGB=0x000000&feed=https%3A%2F%2Fpicasaweb.google.com%2Fdata%2Ffeed%2Fapi%2Fuser%2F100115265397868103538%2Falbumid%2F5622496527675681425%3Falt%3Drss%26kind%3Dphoto%26authkey%3DGv1sRgCICm0Om4m96D1AE%26hl%3Dpl\" pluginspage=\"http://www.macromedia.com/go/getflashplayer\"></embed></center>
+	
+	</div>
+
+");
+
+?>
