a74421b7e2a05e2853e3b3a8f717cd8b5218b92d
mathsymichal <36140040+mathsymichal@users.noreply.github.com>
2018-02-04 22:56:02 +0000

    Add files via upload

---
+++ rainbow_d.php
@@ -0,0 +1,90 @@
+<?
+
+print ("
+
+  <div class=\"newsy\">
+
+  <center>
+    <font color=\"navy\" size=\"4\">
+      <br /><b>Regenbogenbrücke</b>
+    </font>
+  </center><br />
+
+  <center>
+    <b>Sie begleiteten uns kurze Zeit... Zu kurze...</b>
+  </center><br />
+
+    <div class=\"news\">
+	
+	Tragisches Ereigniss, verantwortungslose Menschen... Hat uns im Mai 2016 verlassen. Royal Hero's You Are So Beautiful \"Yulek\".<br /><br />
+
+	<center><img src=\"./yulek0.jpg\"></center><br />	
+    </div>
+
+    <div class=\"news\">
+
+	Danny hat uns im Maerz 2016 verlassen. Wir waren ziemlich lange zusammen... aber so wie so zu kurz. Royal Hero's Fever My Hope \"Danek\".<br /><br />
+
+	<center><img src=\"./danek0.jpg\"></center><br />	
+    </div>
+  
+    <div class=\"news\">
+      Mit Dir hat es angefangen, unsere Kleine... Und wir hoffen, dass es nie zu Ende geht. Xandra vom Oppermann - Schlößchen \"Sanka\", 30.03.2003 - 4.09.2015.<br /><br />
+
+ <center><img src=\"./sana0.jpg\" alt=\"\"></center><br />
+		
+    </div>
+
+    <div class=\"news\">
+
+      \"Papa\" Kentucky Joe du Chateau Noblesse hat uns verlassen... 20.02.2004 – 27.03.2015. Wir sehen uns wieder Papa, bis dahin!<br /><br />
+
+ <center><img src=\"./papa0.jpg\" alt=\"\"></center><br />
+		
+    </div>
+  
+    <div class=\"news\">
+
+      Tymon (Haker Sinkowcoon) zog zu uns im Januar 2009. Ein wunderbarer, unabhengiger Kater... Leider, schon im Fruehling 2014 hat er uns, wegen einem ungluecklichem Unfall, verlassen...<br /><br />
+
+ <center><img src=\"./tymon2.jpg\" alt=\"\"></center><br />
+		
+    </div>
+
+    <div class=\"news\">
+
+      \"Visunia\" (One Vision) kam bei uns auf die Welt als letztes Baby von Norma Jean am 4 April 2012... Eine grauvolle Krankheit hat sie uns schon im September 2013, genommen...<br /><br />
+
+ <center><img src=\"./visa1.jpg\" alt=\"\"></center><br />
+		
+    </div>
+
+    <div class=\"news\">
+
+      \"Fiesta\" Festa z Bursztynowego Rodu (FCI), sie zog bei uns im Juli 2006 ein, nach langer schwerer Krankheit, hat sie uns im April 2012 verlassen.<br /><br />
+
+ <center><img src=\"./fiesta0.jpg\" alt=\"\"></center><br />
+		
+    </div>
+
+    <div class=\"news\">
+
+      \"Judka\" Judka Tarlang Bass, begleitete uns ab Juli 2007... Nach einer kurzer gewaltiger Krankheit verliess sie uns im Mai 2009.<br /><br />
+
+ <center><img src=\"./judka0.jpg\" alt=\"\"></center><br />
+		
+    </div>
+
+    <div class=\"news\">
+
+      Maciek kam zu uns als ein paar Monate junges Kaetzches im Maerz 2004, verlassen hat er uns im Oktober 2008 nach einem dramatischen Unfall.<br /><br />
+
+ <center><img src=\"./maciek0.jpg\" alt=\"\"></center><br />
+		
+    </div>
+
+  </div>
+
+");
+
+?>
---
+++ rainbow_e.php
@@ -0,0 +1,90 @@
+<?
+
+print ("
+
+  <div class=\"newsy\">
+
+  <center>
+    <font color=\"navy\" size=\"4\">
+      <br /><b>Rainbow Bridge</b>
+    </font>
+  </center><br />
+
+  <center>
+    <b>They were here with us for a short time... Too short...</b>
+  </center><br />
+
+    <div class=\"news\">
+
+	Tragic event, human's irresponsibility... He left us in May 2016. Royal Hero's You Are So Beautiful \"Yulek\".<br /><br />
+
+	<center><img src=\"./yulek0.jpg\"></center><br />	
+    </div>
+
+    <div class=\"news\">
+
+	Danek left us in March 2016... We were lucky to spend many years together... but it still felt way too short. Royal Hero's Fever My Hope \"Danek\".<br /><br />
+
+	<center><img src=\"./danek0.jpg\"></center><br />	
+    </div>
+	
+    <div class=\"news\">
+      It all started with you, little girl... And we hope that it will go on. Xandra vom Oppermann - Schlößchen \"Sanka\", 30.03.2003 - 4.09.2015.<br /><br />
+
+ <center><img src=\"./sana0.jpg\" alt=\"\"></center><br />
+		
+    </div>
+
+    <div class=\"news\">
+
+      Our King has passed... \"Papa\" Kentucky Joe du Chateau Noblesse 20.02.2004 - 27.03.2015. See you on the other side Papa!<br /><br />
+
+ <center><img src=\"./papa0.jpg\" alt=\"\"></center><br />
+		
+    </div>
+
+    <div class=\"news\">
+
+      Tymon (Haker Sinkowcoon) arrived at our house in January 2009. A wonderful, clever and independent cat... We sadly lost him in spring 2014, due to an unfortunate accident...<br /><br />
+
+ <center><img src=\"./tymon2.jpg\" alt=\"\"></center><br />
+		
+    </div>
+
+    <div class=\"news\">
+
+      \"Visunia\" (One Vision) was born in our house on the 4th of April 2012. She was the last puppy of Norma Jean. A bad illness took her away in September 2013...<br /><br />
+
+ <center><img src=\"./visa1.jpg\" alt=\"\"></center><br />
+		
+    </div>
+
+    <div class=\"news\">
+
+      \"Fiesta\" Festa z Bursztynowego Rodu (FCI) came to live with us in Wiślinka in February 2006. She died after a long and bad illness in April 2012.<br /><br />
+
+ <center><img src=\"./fiesta0.jpg\" alt=\"\"></center><br />
+		
+    </div>
+
+    <div class=\"news\">
+
+      \"Judka\" Judka Tarlang Bass joined our pack in July 2007... After a short, sudden illness, she left us in May 2009.<br /><br />
+
+ <center><img src=\"./judka0.jpg\" alt=\"\"></center><br />
+		
+    </div>
+
+    <div class=\"news\">
+
+      Maciek, who arrived at our house in March 2004 as a few months old cat from... Malbork (we used to call him our \"Crusader from Malbork\" ;-) ), left us due to an unfortunate accident in October 2008.<br /><br />
+
+ <center><img src=\"./maciek0.jpg\" alt=\"\"></center><br />
+		
+    </div>
+
+  </div>
+
+");
+
+?>
