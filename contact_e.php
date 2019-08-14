79cd1ced66857ba26a8369127b952f213e175ba1
mathsymichal <michal.przykucki@gmail.com>
2019-08-14 20:14:28 +0100

    sd

--- contact_e.php
+++ contact_e.php
@@ -13,7 +13,7 @@ print ("
 		<p>Ewa and Witek Rudzińscy<br />
 ul. Radosna 20<br />
 83-011 Wiślinka<br />
-tel. +48 501 804 615<br />
+tel. +48 730 177 128, +48 730 177 127<br />
 e-mail: <a href=\"mailto:Pomeranian Beauties <ewaiwitek@poczta.fm>\">ewaiwitek@poczta.fm</a></p>
 
 <center><iframe width=\"500\" height=\"400\" frameborder=\"0\" scrolling=\"no\" marginheight=\"0\" marginwidth=\"0\" src=\"http://maps.google.com/maps?q=54.337419,18.798723&amp;hl=en&amp;sll=54.337422,18.798731&amp;sspn=0.000807,0.002642&amp;t=m&amp;ie=UTF8&amp;ll=54.337419,18.798723&amp;spn=0.206669,0.676346&amp;z=10&amp;output=embed\"></iframe><br /><small><a href=\"http://maps.google.com/maps?q=54.337419,18.798723&amp;hl=en&amp;sll=54.337422,18.798731&amp;sspn=0.000807,0.002642&amp;t=m&amp;ie=UTF8&amp;ll=54.337419,18.798723&amp;spn=0.206669,0.676346&amp;z=10&amp;source=embed\" style=\"color:#0000FF;text-align:left\">View Larger Map</a></small></center>
