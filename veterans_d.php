bd28ebe04afbd4b27dbbbb016a01276ac7872a09
mathsymichal <36140040+mathsymichal@users.noreply.github.com>
2023-03-09 22:28:24 +0000

    V puppies update

--- veterans_d.php
+++ veterans_d.php
@@ -6,32 +6,56 @@ print ("
 
   <center>
     <font color=\"navy\" size=\"4\">
-      <br /><b>Unsere Veteranen</b>
+      <br /><b>Our Veterans</b>
     </font>
   </center><br />
-
+	
     <div class=\"news\">
 	<center>
 
-	<font color=\"black\" size=\"5\"><b>CASSANDRA vom Paulinenhof \"Casia\"</b></font><br /><br />
+	<font color=\"black\" size=\"5\"><b>TINTIN Pomeranian Beauties</b></font><br /><br />
 
-	<font color=\"black\" size=\"4\"><b>geb. 13.05.2007 r.</b></font><br /><br />
+	<font color=\"black\" size=\"4\"><b>geb. 28.06.2014</b></font><br /><br />
 
-	<img src=\"./psy/galeria/veterans/casia.jpg\" alt=\"CASSANDRA vom Paulinenhof\"></center><br />
+	<font color=\"navy\" size=\"4\"><b>Interchampion</b></font><br />
+	<font color=\"navy\" size=\"4\"><b>Multi Champion</b></font><br /><br />
+
+	<img src=\"./psy/galeria/veterans/tintin.jpg\" alt=\"TINTIN Pomeranian Beauties\"></center><br />
 	</center>		
     </div>
 
     <div class=\"news\">
 	<center>
 
-	<font color=\"black\" size=\"5\"><b>DAYLIGHT Pomeranian Beauties \"Dayla\"</b></font><br /><br />
+	<font color=\"black\" size=\"5\"><b>SCARLETT O'HARA Pomeranian Beauties \"Clara\"</b></font><br /><br />
 
-	<font color=\"black\" size=\"4\"><b>geb. 26.04.2008 r.</b></font><br /><br />
+	<font color=\"black\" size=\"4\"><b>geb. 30.03.2013</b></font><br /><br />
 
-	<font color=\"navy\" size=\"4\"><b>Polnischer und Russischer Champion</b></font><br />
-	<font color=\"navy\" size=\"4\"><b>Polnischer Jugend Champion</b></font><br /><br />
+	<img src=\"./psy/galeria/veterans/clara.jpg\" alt=\"SCARLETT O'HARA Pomeranian Beauties\"></center><br />
+	</center>		
+    </div>
 
-	<img src=\"./psy/galeria/veterans/dayla.jpg\" alt=\"DAYLIGHT Pomeranian Beauties\"></center><br />
+    <div class=\"news\">
+	<center>
+
+	<font color=\"black\" size=\"5\"><b>TINY DANCER Pomeranian Beauties \"Tini\"</b></font><br /><br />
+
+	<font color=\"black\" size=\"4\"><b>geb. 28.06.2014</b></font><br /><br />
+
+	<font color=\"navy\" size=\"4\"><b>Polnischer und Bulgarischer Champion</b></font><br /><br />
+
+	<img src=\"./psy/galeria/veterans/tini.jpg\" alt=\"TINY DANCER Pomeranian Beauties\"></center><br />
+	</center>		
+    </div>
+
+    <div class=\"news\">
+	<center>
+
+	<font color=\"black\" size=\"5\"><b>UNIQUE Pomeranian Beauties \"Unia\"</b></font><br /><br />
+
+	<font color=\"black\" size=\"4\"><b>geb. 04.07.2014</b></font><br /><br />
+
+	<img src=\"./psy/galeria/veterans/unia.jpg\" alt=\"UNIQUE Pomeranian Beauties\"></center><br />
 	</center>		
     </div>
 
